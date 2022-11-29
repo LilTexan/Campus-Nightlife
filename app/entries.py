3088d54e718eacc87de8bfc3dda8290a4c332676
LilTexan <timstansberry0@gmail.com>
2022-11-28 20:04:25 -0500

    Tested google sheets access, functional for now.

--- app/entries.py
+++ app/entries.py
@@ -1,5 +1,47 @@
 import random
 import plotly.express as px
+import os
+from dotenv import load_dotenv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+load_dotenv()
+
+def authorizeGoogleSheets():
+    DOCUMENT_ID = os.getenv("GOOGLE_SHEET_ID", default="OOPS")
+    SHEET_NAME = os.getenv("SHEET_NAME", default="Responses")
+
+    CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "google-credentials.json")
+
+    AUTH_SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
+        "https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
+    ]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+    print("CREDS:", type(credentials)) #> <class 'oauth2client.service_account.ServiceAccountCredentials'>
+
+    client = gspread.authorize(credentials)
+    print("CLIENT:", type(client)) #> <class 'gspread.client.Client'>
+
+    print("-----------------")
+    print("READING DOCUMENT...")
+
+    # access the document:
+    doc = client.open_by_key(DOCUMENT_ID)
+    print("DOC:", type(doc), doc.title) #> <class 'gspread.models.Spreadsheet'>
+
+    # access a sheet within the document:
+    sheet = doc.worksheet(SHEET_NAME)
+    print("SHEET:", type(sheet), sheet.title)#> <class 'gspread.models.Worksheet'>
+
+    # fetch all data from that sheet:
+    rows = sheet.get_all_records()
+    print("ROWS:", type(rows)) #> <class 'list'>
+
+    # loop through and print each row, one at a time:
+    for row in rows:
+        print(row) #> <class 'dict'>
 
 #Defining essential lists and functions
 clubList = ["Abigail", "Sign of the Whale", "Tokyo Pearl", "Ultrabar", "Decades"]
@@ -11,17 +53,18 @@ def addEntry (entry):
         raise Exception("Incorrect entry syntax in addEntry")
     entries.append({"club": entry[0], "year": entry[1]})
 
-
-if __name__ == "__main__":
-    #Generating randomized entries
+def generateEntries(length):
     x = 0
-    while x < 50:
+    while x < length:
         club = random.choice(clubList)
         year = random.choice(yearList)
         addEntry([club, year])
         x += 1
 
-    print(entries)
+
+if __name__ == "__main__":
+    generateEntries(50)
+    authorizeGoogleSheets()
 
     #Generating output graphics
     clubFrequency = [0, 0, 0, 0, 0]
