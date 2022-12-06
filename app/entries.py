b50706b5930de98837f2bc0ca25dc8610f21f588
LilTexan <timstansberry0@gmail.com>
2022-12-05 20:35:55 -0500

    Fixed syntax error

--- app/entries.py
+++ app/entries.py
@@ -24,7 +24,7 @@ def generateEntries(length):
 
 def authorizeGoogleSheets():
     #CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "google-credentials.json")
-    CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__),'/../google-credentials.json'))
+    CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__),'/../google-credentials.json')
 
     AUTH_SCOPE = [
         "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
